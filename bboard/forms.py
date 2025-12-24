0cae1564df8d9b28fbc54037859e916a0bb5110f
sara1234567-ask <saraaskarova73@gmail.com>
2025-12-24 23:05:04 +0500

    Init

--- bboard/forms.py
+++
@@ -1,9 +0,0 @@
-from django.forms import ModelForm
-
-from bboard.models import Bb, Rubric
-
-
-class BbForm(ModelForm):
-    class Meta:
-        model = Bb
-        fields = ('title', 'content', 'price', 'rubric')
--- main/apps.py
+++ main/apps.py
@@ -1,7 +1,6 @@
 from django.apps import AppConfig
 
 
-class BboardConfig(AppConfig):
+class MainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'bboard'
-    verbose_name = 'Объявления'
+    name = 'main'
